64b1ce6ed634c7c2956c3ca49e0926c7ef5d2ed3
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-20 02:35:11 +0000

    Adding tarfile member sanitization to extractall()

--- pesto-common/pesto_common/utils/file_utils.py
+++ pesto-common/pesto_common/utils/file_utils.py
@@ -89,7 +89,26 @@ class FileUtils(object):
     @staticmethod
     def unpack_gz(filename, dest_dir):
         with tarfile.open(filename, "r:gz") as tar:
-            tar.extractall(path=dest_dir)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, path=dest_dir)
 
     '''
     add pickel
